f7182bab8acc600ddeb7aed439c2ff7dfa8774f0
Ivana Markovic <34767748+ickaim@users.noreply.github.com>
2018-09-07 17:57:36 +0200

    Update contact.php

--- includes/contact.php
+++ includes/contact.php
@@ -1,15 +1,16 @@
  <div id="contact">
-        <div class="blank"></div>
-        <div class="contact">
-           <div>
-            <img src="Slika_adrese.jpg" alt=""></div>
-            <div class="contact_text">
-            <h1>CONTACT</h1><br>
-            <p>Bulevar Petra Cetinjskog 56</p>
-            <p>81000 Podgorica, Montenegro</p><br>
-            <p>+382 223 240</p><br>
-            <p>info@amplitudo.me</p>
-            </div>
+    <div class="blank"></div>
+    <div class="contact">
+       <div id="map" >
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2944.2541201567174!2d19.247660314843802!3d42.44360893781494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134deb305185d703%3A0x3864f3a9972de2d4!2sAmplitudo+DOO!5e0!3m2!1sen!2s!4v1536264393947" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
-        <div class="blank"></div>
+        <div class="contact_text">
+        <h1>CONTACT</h1><br>
+        <p>Bulevar Petra Cetinjskog 56</p>
+        <p>81000 Podgorica, Montenegro</p><br>
+        <p>+382 223 240</p><br>
+        <p>info@amplitudo.me</p>
+        </div>
     </div>
+    <div class="blank"></div>
+</div>
